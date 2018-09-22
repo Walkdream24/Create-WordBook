7628583c78c88826e9403220f787a8f68de745cb
github Walkdream24 <walkdream24@gmail.com>
2018-09-22 16:46:55 +0900

    tabBar improve

---
+++ Create WordBook/CustomTabBarController.swift
@@ -0,0 +1,31 @@
+//
+//  CustomTabBarController.swift
+//  Create WordBook
+//
+//  Created by 中重歩夢 on 2018/09/21.
+//  Copyright © 2018年 Ayumu Nakashige. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+       
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
